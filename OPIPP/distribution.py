babf7550e24a5c3d10da89cd312b96394349aa9f
heliy <ohhlyhly@gmail.com>
2023-07-03 18:46:22 +0800

    merge mosaic & graph -> mosaic

---
+++ OPIPP/distribution.py
@@ -0,0 +1,76 @@
+import numpy as np
+
+class Distribution:
+    """ 
+    Probability distribution of feature values
+        
+    Parameters
+    ----------
+    max_value: float
+        The maximum value for bins in histogram.
+
+    n_bin: int
+        The number of bins in histogram.
+
+    target_probs: list or np.ndarray
+        The value of temperature at initalization.
+
+    min_value: float, optional(default=0.)
+        The minimum value for bins in histogram.
+
+    Methods
+    -------
+    get_prob()
+        Gets the probability of a given value.
+
+    get_hist()
+        Gets the histrogram of probablities of given values.
+
+    KL()
+        Calculates the KL divergency between probabllities of given values and the target.
+    
+    sample_values()
+        Generates random values following the target probabilities.
+    
+    sample_ri()
+        Calculate the Regularity Index of values generated randomly.
+    """
+    def __init__(self, max_value: float, n_bin: int, target_probs: list, min_value: float=0.):
+        assert len(target_probs) == n_bin+1
+        assert max_value > min_value
+        self.max_value = max_value
+        self.min_value = min_value
+        self.n_bin = n_bin
+        self.target_probs = target_probs
+        # make sure the correct calculation for KL distance
+        self.target_probs[self.target_probs<=0] = 1e-5 
+        self.target_probs /= self.target_probs.sum()
+        self.step = (max_value-min_value)/n_bin
+
+    def __get_index(self, value: float) -> int:
+        return min(int((value-self.min_value)/self.step), self.n_bin)
+
+    def get_prob(self, value: float) -> float:
+        return self.target_probs[self.__get_index(value)]
+
+    def get_value_centers(self) -> np.ndarray:
+        return np.linspace(self.min_value, self.max_value, num=self.n_bin+1)+self.step/2
+
+    def get_hist(self, values: np.ndarray) -> np.ndarray:
+        hist, _ = np.histogram(values, bins=self.n_bin, range=(self.min_value, self.max_value))
+        hist = np.concatenate((hist, [len(values)-sum(hist)])).astype(float)
+        return hist
+
+    def KL(self, distances: np.ndarray) -> float:
+        hist = self.get_hist(distances)
+        probs = hist/hist.sum()
+        kl = np.sum([pk * np.log(pk / qk) for pk, qk in zip(probs, self.target_probs) if qk > 0 and pk > 0])
+        return kl
+
+    def sample_values(self, n: int=1) -> np.ndarray:
+        centers = np.random.choice(self.get_value_centers(), p=self.target_probs, size=n)
+        return (np.random.rand(n)-0.5)*self.step+centers
+
+    def sample_ri(self, n_sample=50000) -> float:
+        values = self.sample_values(n_sample)
+        return values.mean()/values.std()
--- OPIPP/mosaic.py
+++ OPIPP/mosaic.py
@@ -1,15 +1,15 @@
-#coding:UTF-8
-
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.spatial.distance import euclidean
+from scipy.spatial.qhull import Delaunay
+from scipy.spatial import Voronoi
 from scipy.spatial import voronoi_plot_2d
 import networkx as nx
 
-from .utils import get_poly_centeroid
+from .utils import get_poly_area, get_poly_centeroid
 from .scope import Scope
-from .graph import PointNetwork
 
-class Mosaic:
+class Mosaic(nx.Graph):
     """ 
     A mosaic with given locations.
         
@@ -21,44 +21,171 @@ class Mosaic:
     scope: Scope
         the area of the mosaic.
     """
-    def __init__(self, points: np.ndarray, scope: Scope):
+    def __init__(self, points: np.ndarray, scope: Scope, **attr):
         self.points = np.array(points)
         assert len(self.points.shape) == 2
         self.scope = scope
-        self.pnet = PointNetwork(points=self.points, scope=self.scope)
+        self.__set_effective_filter()
 
+        # add points mirroring with inner points by edges to ensure the correctness of voronoi domains
+        appended_points = []
+        effective_filter = self.get_effective_filter()
+        for index, is_effective in enumerate(effective_filter):
+            if not is_effective:
+                reflect_points = self.scope.get_reflect_points(self.points[index])
+                appended_points.extend(reflect_points)
+        appended_points = np.array(appended_points)
+        appended_points = appended_points[self.scope.filter(appended_points, not_in=True)]
+        all_points = self.points.tolist()+appended_points.tolist()
+        triangle = Delaunay(all_points)
+        self.extended_vor = Voronoi(all_points, qhull_options='Qbb Qc Qx')
+
+        # build the network of points without reflected points
+        maxN = self.points.shape[0]
+        for path in triangle.simplices:
+            a, b, c = tuple(path)
+            if a < maxN and b < maxN:
+                nx.add_path(self, [a, b])
+            if b < maxN and c < maxN:
+                nx.add_path(self, [b, c])
+            if a < maxN and c < maxN:
+                nx.add_path(self, [a, c])
+        for u, v in self.edges():
+            distance = euclidean(self.points[u], self.points[v])
+            self.edges[u, v]["length"] = distance
+
+    def __edge_length(self, u, v):
+        try:
+            return self.edges[u, v]["length"] 
+        except:
+            return self.edges[v, u]["length"]
+        
     def get_points_n(self):
         return self.points.shape[0]
 
+    ########################################
+    #
+    # Methods for boundary effects
+    #
+    ########################################
+
+    def __set_effective_filter(self):
+        vor = Voronoi(self.points, qhull_options='Qbb Qc Qx')
+        outs = self.scope.filter(vor.vertices, not_in=True)
+        outs = set(list(outs))
+        self.effective_filter = np.zeros(self.points.shape[0]).astype(bool)
+        for i in range(self.points.shape[0]):
+            region = list(vor.regions[vor.point_region[i]])
+            if len(region) > 0 and -1 not in region and len(set(region)&outs) == 0:
+                self.effective_filter[i] = True
+
+    def get_effective_filter(self) -> np.ndarray:
+        return self.effective_filter
+    
     def iter_effective_indices(self):
-        for p_index, is_effective in enumerate(self.pnet.get_effective_filter()):
+        for p_index, is_effective in enumerate(self.get_effective_filter()):
             if is_effective:
                 yield p_index
 
-    def get_effective_nns(self):
-        nns = []
-        for p_index, is_effective in enumerate(self.pnet.get_effective_filter()):
-            if is_effective:
-                _, nn = self.pnet.find_nearest_neighbor(p_index)
-            nns.append(nn)
-        return np.array(nns)
+    def get_boundary_indices(self):
+        """ Gets indices of boundary points """
+        in_surrounds = np.arange(self.points.shape[0])[1-self.get_effective_filter()]
+        return in_surrounds
+    
+    ########################################
+    #
+    # Feature-related methods
+    #
+    ########################################
 
-    def get_effective_vorareas(self):
-        return np.array(self.pnet.get_vorareas(effective_filter=True))
+    def __get_features(self, feauture_func, indices: np.ndarray=None, effective_filter: bool=True) -> list:
+        if indices is None or len(indices) == 0:
+            indices = range(self.points.shape[0])
+        if effective_filter:
+            node_filter = self.get_effective_filter()
+        else:
+            node_filter = np.ones(self.points.shape[0]).astype(bool)
+        values = []
+        for index in indices:
+            if node_filter[index]:
+                feature = feauture_func(index)
+                values.append(feature)
+        return values
 
-    def nnri(self):
-        distances = self.get_effective_nns()
-        return distances.mean()/distances.std()
+    def get_vorareas(self, indices: np.ndarray=None, effective_filter: bool=True) -> list:
+        """ 
+        get values of voronoi areas
+        
+        Parameters
+        ----------
+        indices: dict or list, optional(default=None)
+            The indices of cells to query. Without specification, it will return values of all cells.
 
-    def vdri(self):
-        areas = self.get_effective_vorareas()
-        return areas.mean()/areas.std()
+        effective_filter: bool, optional(default=True)
+            If True, it will exclude boundary cells.
+        """
+        def vorarea_func(index):
+            region = self.extended_vor.regions[self.extended_vor.point_region[index]]
+            region = self.extended_vor.vertices[region]
+            area = get_poly_area(region[:, 0], region[:, 1])
+            return area
+        return self.__get_features(feauture_func=vorarea_func, indices=indices, effective_filter=effective_filter)
 
-    def find_neighbors(self, p_index, effective_only=False):
-        neighbors = self.pnet.find_neighbors(p_index)
+    def find_neighbors(self, p_index, effective_only=False) -> list:
+        neighbors = list(self.neighbors(p_index))
         if effective_only:
             return list(set(neighbors).difference(set(self.pnet.get_boundary_indices())))
         return neighbors
+
+    def find_nearest_neighbor(self, node: int) -> tuple:
+        """ Gets its NN neighbor and NN distance of a given cell """
+        min_distance = self.scope.get_area()
+        min_neighbor = -1
+        for neighbor in self.find_neighbors():
+            distance = self.__edge_length(neighbor, node)
+            if distance < min_distance:
+                min_distance = distance
+                min_neighbor = neighbor
+        return min_neighbor, min_distance
+
+    def get_nn_graph(self, nodes=None) -> nx.DiGraph:
+        """ 
+        Gets a directed subgraph containing all points and NN edges 
+        """
+        if nodes is None:
+            nodes = self.nodes
+        graph = nx.DiGraph()
+        graph.add_nodes_from(self.nodes)
+        for node in nodes:
+            nn_neighbor, _ = self.find_nearest_neighbor(node)
+            graph.add_edge(node, nn_neighbor)
+        return graph
+
+    def get_nns(self, indices: np.ndarray=None, effective_filter: bool=True) -> list:
+        """ 
+        get values of nearest neighbor distances
+        
+        Parameters
+        ----------
+        indices: dict or list, optional(default=None)
+            The indices of cells to query. Without specification, it will return values of all cells.
+
+        effective_filter: bool, optional(default=True)
+            If True, it will exclude boundary cells.
+        """
+        def nn_func(index):
+            _, nn_distance = self.find_nearest_neighbor(index)
+            return nn_distance
+        return self.__get_features(feauture_func=nn_func, indices=indices, effective_filter=effective_filter)
+
+    def get_distances(self, indices: list=None):
+        """ Gets distances of edges related to given cells, all edges if not specific """
+        if indices is not None:
+            edge_iter = self.edges(indices)
+        else:
+            edge_iter = self.edges()
+        distances = [self.__edge_length(u, v) for u, v in edge_iter]
+        return np.array(distances)
         
     ########################################
     #
@@ -104,7 +231,6 @@ class Mosaic:
         plt.xlim([-lim, lim])
         plt.ylim([-lim, lim])
         plt.show()
-
 
     def draw_points(self, highlights=None, grid=1, color='r', size=5):
         # background
@@ -166,3 +292,6 @@ class Mosaic:
         plt.ylim([self.scope.min_y, self.scope.max_y])
         plt.show()
     
+    def draw_raw(self, edge_color='r', node_color='b', node_size=5):
+        nx.draw_networkx(self, pos=self.points, edge_color=edge_color, node_color=node_color, node_size=node_size)
+        plt.show()
